e943c977b1c5ac9f74a43ab6dd8d5a4682935d21
Danya Sherbini <danyasherbini9@gmail.com>
2025-06-16 14:00:45 -0500

    prep for deployment

--- frontend/src/pages/Landscape.jsx
+++ frontend/src/pages/Landscape.jsx
@@ -13,8 +13,8 @@ export default function Landscape() {
   const [displayedData, setDisplayedData] = useState([]); // holds filtered/memoized data from table
 
   useEffect(() => {
-    //fetch("https://data-work-landscape-lymyf.ondigitalocean.app/ai-data-work-landscape-backend/data") // for production
-    fetch("http://localhost:8000/data") // for development
+    fetch("https://data-work-landscape-lymyf.ondigitalocean.app/ai-data-work-landscape-backend/data") // for production
+    //fetch("http://localhost:8000/data") // for development
       .then((res) => res.json())
       .then(setData);
   }, []);
