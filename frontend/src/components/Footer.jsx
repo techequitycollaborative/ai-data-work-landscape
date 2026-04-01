0f4a303e98b4e0d79e3296841d0d37d2cf1c6dc0
Danya Sherbini <danyasherbini9@gmail.com>
2026-04-01 14:14:31 -0500

    Update Footer.jsx

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -7,7 +7,7 @@ export default function Footer() {
       id="myFooter"
       className="bg-white text-center text-gray-400 py-6 border-t mt-10">
         <p>
-          <a href="https://techequity.us/privacy-policy/" className="hover:text-gray-800" target="_blank" rel="noopener noreferrer">Privacy Policy</a>    |    <a href=" https://techequity.us/code-of-conduct/" className="hover:text-gray-800" target="_blank" rel="noopener noreferrer">Terms of Use</a>    |    © 2025 TechEquity. All Rights Reserved.
+          <a href="https://techequity.us/privacy-policy/" className="hover:text-gray-800" target="_blank" rel="noopener noreferrer">Privacy Policy</a>    |    <a href=" https://techequity.us/code-of-conduct/" className="hover:text-gray-800" target="_blank" rel="noopener noreferrer">Terms of Use</a>    |    © 2026 TechEquity. All Rights Reserved.
         </p>
       </footer>
     );
