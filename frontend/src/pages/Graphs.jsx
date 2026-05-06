0c5afa4691afafa0d7ca0a64977e33ffab663cfa
Danya Sherbini <danyasherbini9@gmail.com>
2026-05-06 14:55:30 -0500

    update graphs disclaimer

--- frontend/src/pages/Graphs.jsx
+++ frontend/src/pages/Graphs.jsx
@@ -37,13 +37,17 @@ export default function Graphs() {
       {/* Expander with disclaimer */}
       <div className="w-full px-12 md:px-20 mt-4">
         <Expander title="A note about these graphs">
+        <div className="space-y-4">
         <p>
-            Data work is an opaque industry. Companies are generally not required to reveal contractual relationships and these contracts shift constantly. 
-            In the absence of transparent information, this resource is based on an incomplete set of publicly available sources including news coverage, 
-            academic and trade publications, and company websites. The landscape provides a snapshot of some of the companies that employ data workers and 
-            their relationships, but this resource is not – and cannot be – exhaustive or completely up-to-date. Visit our methods page for more information 
-            on our methodology and limitations.       
+          Data work is an opaque industry. Companies are generally not required to reveal contractual relationships and these contracts shift constantly. In the absence of transparent information, these graphs are based on an incomplete set of publicly available sources including news coverage, academic and trade publications, and company websites.
         </p>
+        <p>
+          The graphs provide a snapshot of some of the companies that employ data workers and their relationships, but this resource is not—and cannot be—exhaustive or completely up-to-date.
+        </p>
+        <p>
+          Visit our <a href="https://dataworklandscape.org/about" className="text-blue-600 underline hover:opacity-70 transition-all">About</a> page for more information on our methodology and limitations.
+        </p>
+        </div>
         </Expander>
     </div>
 
